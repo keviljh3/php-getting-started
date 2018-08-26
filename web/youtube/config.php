bb8eb8f35e340ffc07429a8cc3c3e343988dc808
keviljh3 <kevinljh3@hotmail.com>
2018-08-26 09:36:30 +0800

    Add files via upload

---
+++ web/youtube/config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', 'AIzaSyDbcdU3bkNzerzkoiL8mwCFUSQpKskZW2w');
+define('GJ_CODE', 'TW');
+define('SITE_NAME', 'ABC');
+define('TITLENAME', '000');
+define('EN2DEKEY', 'abc1234567890');
+define('EMAIL', 'kevinljh1@hotmail.com');
+?>
