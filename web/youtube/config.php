8c7d22bcefcfffbaaf9d4057b7ba167196e1de17
keviljh3 <kevinljh3@hotmail.com>
2018-08-26 09:41:30 +0800

    Update config.php

--- web/youtube/config.php
+++ web/youtube/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyDbcdU3bkNzerzkoiL8mwCFUSQpKskZW2w');
-define('GJ_CODE', 'TW');
+define('GJ_CODE', 'HK');
 define('SITE_NAME', 'ABC');
 define('TITLENAME', '000');
 define('EN2DEKEY', 'abc1234567890');
