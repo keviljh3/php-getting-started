417df7daa71a3735403d0f7ad321fca1d5d54a8c
keviljh3 <kevinljh3@hotmail.com>
2019-02-12 17:27:30 +0800

    Add files via upload

---
+++ web/vip/v.php
@@ -0,0 +1,42 @@
+<?php
+//https://v.qq.com/x/list/tv
+//https://v.qq.com/channel/tv?listpage=1&channel=tv&sort=18&_all=1
+//https://v.qq.com/x/list/tv?offset=0&iarea=814
+//href="?offset=
+error_reporting(0);
+$cs=$_SERVER["QUERY_STRING"];
+if ($cs=="" || $cs==null)
+{
+	$cs="https://v.qq.com/x/list/tv";
+}
+$vdata0=str_replace("http","http://x3.713713.xyz/vip/v.php?http",http_get_data($cs));
+$vdata1=str_replace("?offset=","http://x3.713713.xyz/vip/v.php?https://v.qq.com/x/list/tv?offset=",$vdata0);
+$vdata=str_replace('href="/x/cover/','href="http://x3.713713.xyz/vip/vip3.php?p=https://v.qq.com/x/cover/',$vdata1);
+
+function g_contents($url) {
+    $ch= curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($ch, CURLOPT_TIMEOUT,30);
+    curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0');
+    curl_setopt($ch, CURLOPT_ENCODING ,'');
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    @$data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
+function http_get_data($url) {      
+        $ch = curl_init ();  
+        curl_setopt ( $ch, CURLOPT_CUSTOMREQUEST, 'GET' );  
+        curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, false );  
+        curl_setopt ( $ch, CURLOPT_URL, $url );  
+        ob_start ();  
+        curl_exec ( $ch );  
+        $return_content = ob_get_contents ();  
+        ob_end_clean ();         
+        $return_code = curl_getinfo ( $ch, CURLINFO_HTTP_CODE );  
+        return $return_content;  
+} 
+//header('Content-type: application/json');
+echo $vdata;
+?>
